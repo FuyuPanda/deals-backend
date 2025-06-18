87d8edc57f44aa4c2d14889023f268be9a83044e
Panji Bagaskoro <panjibagaskoro08@gmail.com>
2025-06-18 20:24:15 +0700

    add employeehandlers

--- handlers/employeeHandler.go
+++ handlers/employeeHandler.go
@@ -3,9 +3,25 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
-	"strconv"
 
 	"github.com/FuyuPanda/deals-backend/db"
 	"github.com/FuyuPanda/deals-backend/models"
-	"github.com/go-chi/chi/v5"
+	"golang.org/x/crypto/bcrypt"
 )
+
+func createEmployee(w http.ResponseWriter, r *http.Request) {
+	var employees models.Employees
+	if err := json.NewDecoder(r.Body).Decode(&employees); err != nil {
+		http.Error(w, "Invalid Input", http.StatusBadRequest)
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(employees.Password), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, "Error encrypting password", http.StatusBadRequest)
+	}
+	employees.Password = string(hashedPassword)
+	if err := db.DB.Create(&employees).Error; err != nil {
+		http.Error(w, "Failed to create user", http.StatusInternalServerError)
+		return
+	}
+}
